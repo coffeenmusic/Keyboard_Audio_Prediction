62048b972ec020970fcdc76e92f6bb67884cf284
Stephen Thompson <stephen.revel.thompson@gmail.com>
2018-07-07 23:37:18 -0400

    Moved to class. Only saving wav, not yet logging to dataframe

--- test.py
+++
@@ -1,9 +0,0 @@
-from pynput import keyboard
-
-def on_press(key):
-    print(key)
-    if key == keyboard.Key.esc:
-        return False
-
-with keyboard.Listener(on_press=on_press) as listener:
-    listener.join()
